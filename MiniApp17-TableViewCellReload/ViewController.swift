81b1a59f3e2590a98e7dcf4621a5cc231a38f905
MaedaKouta <maedakoutaa@gmail.com>
2022-02-26 00:17:31 +0900

    commit

--- MiniApp17-TableViewCellReload/ViewController.swift
+++ MiniApp17-TableViewCellReload/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var randomArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshTableView), for: UIControl.Event.valueChanged)
+        tableView.refreshControl = refreshControl
+    }
+
+    @objc func refreshTableView() {
+        randomArray = randomArray.map { $0 * 5 }
+        tableView.refreshControl?.endRefreshing()
+        tableView.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return randomArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(randomArray[indexPath.row])"
+
+        return cell
     }
 
 
